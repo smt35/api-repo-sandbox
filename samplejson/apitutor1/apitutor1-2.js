02c96bf565320c37c47d0e1a5d58aa01a3d9e0d4
Doug Walker <djw5@byu.edu>
2015-03-27 14:11:25 -0600

    Adjust for version issues

---
+++ samplejson/apitutor1/apitutor1-2.js
@@ -0,0 +1,20 @@
+{ "cars": [
+    { "vin": "73",
+      "plate_state": "MN",
+      "plate_number": "669GLG",
+      "vehicle_type": "Truck/SUV",
+      "vehicle_year": "2012",
+      "make": "Subaru",
+      "model": "",
+      "color": "Dark Blue",
+      "owner_id": "485720485",
+      "owner_name": "Miss Daisy",
+      "created_by_id": "485720485",
+      "created_by_name": "Miss Daisy",
+      "created_date_time": "2014-05-11T13:45:53.09Z",
+      "updated_by_id": "783783783",
+      "updated_by_name": "Daisy Duck",
+      "updated_date_time": "2014-09-15T08:12:44.03Z"
+    }
+  ]
+}
