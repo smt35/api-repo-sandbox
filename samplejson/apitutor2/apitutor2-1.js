097628c9062b0fd3ae49c460decbe16b028bbb39
Doug Walker <djw5@byu.edu>
2015-03-30 15:26:20 -0600

    Add Example #2 sample jsons

---
+++ samplejson/apitutor2/apitutor2-1.js
@@ -0,0 +1,146 @@
+{ "cars": {
+    "links": [
+      { "rel": "self",
+        "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars",
+        "method": "GET"
+      },
+      { "rel": "tutorial2-cars.create",
+        "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars",
+        "method": "POST"
+      }
+    ],
+    "metadata": {
+      "collection_size": 3,
+      "page_start": 1,
+      "page_end": 3,
+      "page_size": 50,
+      "default_page_size": 50,
+      "max_page_size": 1000
+    },
+    "values": [
+      { "links": [
+          { "rel": "self",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1FTBE14A4NP05962",
+            "method": "GET"
+          },
+          { "rel": "tutorial2-cars.modify",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1FTBE14A4NP05962",
+            "method": "PUT"
+          },
+          { "rel": "tutorial2-cars.sell",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1FTBE14A4NP05962",
+            "method": "PUT"
+          },
+          { "rel": "tutorial2-cars.delete",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1FTBE14A4NP05962",
+            "method": "DELETE"
+          },
+          { "rel": "tutorial2-owners.info",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-owners/783783783",
+            "method": "GET"
+          }
+        ],
+        "basic": {
+          "vin": "1FTBE14A4NP05962",
+          "plate_state": "UT",
+          "plate_number": "Q580RT",
+          "vehicle_type": "Truck/SUV",
+          "vehicle_year": "2013",
+          "make": "Toyota",
+          "model": "RAV4",
+          "color": "Dark Green",
+          "owner_id": "783783783",
+          "owner_name": "Daisy Duck",
+          "created_by_id": "586586586",
+          "created_by_name": "Daffy Duck",
+          "created_date_time": "2014-05-11T13:45:53.09Z",
+          "updated_by_id": "783783783",
+          "updated_by_name": "Daisy Duck",
+          "updated_date_time": "2014-09-15T08:12:44.03Z"
+        }
+      },
+      { "links": [
+          { "rel": "self",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1DTGR14A6NP38861",
+            "method": "GET"
+          },
+          { "rel": "tutorial2-cars.modify",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1DTGR14A6NP38861",
+            "method": "PUT"
+          },
+          { "rel": "tutorial2-cars.sell",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1DTGR14A6NP38861",
+            "method": "PUT"
+          },
+          { "rel": "tutorial2-cars.delete",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1DTGR14A6NP38861",
+            "method": "DELETE"
+          },
+          { "rel": "tutorial2-owners.info",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-owners/586586586",
+            "method": "GET"
+          }
+        ],
+        "basic": {
+          "vin": "1DTGR14A6NP38861",
+          "plate_state": "UT",
+          "plate_number": "C49D3T",
+          "vehicle_type": "Truck/SUV",
+          "vehicle_year": "1997",
+          "make": "Ford",
+          "model": "Ranger",
+          "color": "Blue",
+          "owner_id": "586586586",
+          "owner_name": "Daffy Duck",
+          "created_by_id": "586586586",
+          "created_by_name": "Daffy Duck",
+          "created_date_time": "2014-05-11T13:45:53.09Z",
+          "updated_by_id": "783783783",
+          "updated_by_name": "Daisy Duck",
+          "updated_date_time": "2014-09-15T08:12:44.03Z"
+        }
+      },
+      { "links": [
+          { "rel": "self",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1CRTW25A3NX78232",
+            "method": "GET"
+          },
+          { "rel": "tutorial2-cars.modify",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1CRTW25A3NX78232",
+            "method": "PUT"
+          },
+          { "rel": "tutorial2-cars.sell",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1CRTW25A3NX78232",
+            "method": "PUT"
+          },
+          { "rel": "tutorial2-cars.delete",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/1CRTW25A3NX78232",
+            "method": "DELETE"
+          },
+          { "rel": "tutorial2-owners.info",
+            "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-owners/586586586",
+            "method": "GET"
+          }
+        ],
+        "basic": {
+          "vin": "1CRTW25A3NX78232",
+          "plate_state": "UT",
+          "plate_number": "L43S3T",
+          "vehicle_type": "Truck/SUV",
+          "vehicle_year": "2003",
+          "make": "Chevrolet",
+          "model": "Sierra",
+          "color": "Tan",
+          "owner_id": "586586586",
+          "owner_name": "Daffy Duck",
+          "created_by_id": "586586586",
+          "created_by_name": "Daffy Duck",
+          "created_date_time": "2014-05-11T13:45:53.09Z",
+          "updated_by_id": "783783783",
+          "updated_by_name": "Daisy Duck",
+          "updated_date_time": "2014-09-15T08:12:44.03Z"
+        }
+      }
+    ]
+  }
+}
---
+++ samplejson/apitutor2/apitutor2-3.js
@@ -0,0 +1,43 @@
+{ "car": {
+    "links": [
+      { "rel": "self",
+        "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/73",
+        "method": "GET"
+      },
+      { "rel": "tutorial2-cars.modify",
+        "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/73",
+        "method": "PUT"
+      },
+      { "rel": "tutorial2-cars.sell",
+        "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/73",
+        "method": "PUT"
+      },
+      { "rel": "tutorial2-cars.delete",
+        "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-cars/73",
+        "method": "DELETE"
+      },
+      { "rel": "tutorial2-owners.info",
+        "href": "https://edge-wso2.byu.edu:3000/byuapi/tutorial2-owners/783783783",
+        "method": "GET"
+      }
+    ],
+    "basic": {
+      "vin": "73",
+      "plate_state": "UT",
+      "plate_number": "Q580RT",
+      "vehicle_type": "Truck/SUV",
+      "vehicle_year": "2013",
+      "make": "Toyota",
+      "model": "RAV4",
+      "color": "Dark Green",
+      "owner_id": "783783783",
+      "owner_name": "Daisy Duck",
+      "created_by_id": "586586586",
+      "created_by_name": "Daffy Duck",
+      "created_date_time": "2014-05-11T13:45:53.09Z",
+      "updated_by_id": "783783783",
+      "updated_by_name": "Daisy Duck",
+      "updated_date_time": "2014-09-15T08:12:44.03Z"
+    }
+  }
+}
