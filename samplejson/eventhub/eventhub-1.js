eacbe897ec38e150e7ffa082fa3e2a1611c61544
mhailstone <matthew.hailstone@gmail.com>
2015-04-29 16:06:05 -0600

    Added example response for the Try It button.

---
+++ samplejson/eventhub/eventhub-1.js
@@ -0,0 +1,79 @@
+{
+    "events" : {
+        "event" : [
+            {
+                "event_header": {
+                    "domain": "byu.edu",
+                    "entity": "AIM WaitListing",
+                    "event_type": "Add Class",
+                    "source_dt": "2014-03-02T10:24:13.68Z",
+                    "event_dt": "2014-03-02T10:24:13.79Z",
+                    "source_id": "3468493",
+                    "event_id": "bfa5a232-dfe9-2cda-4bc5-ae3c22ea7231",
+                    "dispatch_id": "bcd2e432-dfe8-3bcb-6a2a-ead9bbe586934"
+                },
+                "filters": {
+                    "filter": [
+                        {
+                            "filter_name": "college",
+                            "filter_value": "1017"
+                        },
+                        {
+                            "filter_name": "department",
+                            "filter_value": "1326"
+                        },
+                        {
+                            "filter_name": "year_term",
+                            "filter_value": "20141"
+                        },
+                        {
+                            "filter_name": "curriculum_id",
+                            "filter_value": "06387"
+                        },
+                        {
+                            "filter_name": "title_code",
+                            "filter_value": "001"
+                        },
+                        {
+                            "filter_name": "teaching_area",
+                            "filter_value": "MATH"
+                        },
+                        {
+                            "filter_name": "catalog_number",
+                            "filter_value": "110"
+                        },
+                        {
+                            "filter_name": "section_number",
+                            "filter_value": "001"
+                        }
+                    ]
+                },
+                "event_body": {
+                    "person_id": "585241162",
+                    "student_name": "Ithaca, Bugs Bunny",
+                    "year_term": "20141",
+                    "curriculum_id": "06387",
+                    "title_code": "001",
+                    "section_number": "001",
+                    "teaching_area": "MATH",
+                    "catalog_number": "110",
+                    "credit_hours": "3.0"
+                },
+                "history": {
+                    "event_header": [
+                        {
+                            "domain": "byu.edu",
+                            "entity": "MyMap",
+                            "event_type": "Drop Class",
+                            "source_dt": "2014-03-02T10:21:45.32Z",
+                            "event_dt": "2014-03-02T10:21:49.36Z",
+                            "source_id": "AB-45834",
+                            "event_id": "cfa6a732-dce9-2bac-3bc5-ad3cff657231",
+                            "dispatch_id": "bcd2e432-dfe8-3bcb-6a2a-ead9bbe586934"
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+}
