7a42af4b8fb23e4e6f9ae3ec2cf737c632a9f981
mhailstone <matthew.hailstone@gmail.com>
2015-04-30 17:31:55 -0600

    Added events to event hub API

---
+++ samplejson/eventhub/eventhub-8.js
@@ -0,0 +1,32 @@
+{
+    "events_return": {
+        "event_return": [
+            {
+                "domain": "edu.byu",
+                "entity": "AIM WaitListing",
+                "event_type": "Add Class",
+                "source_dt": "2014-05-11T13:45:53.20Z",
+                "event_dt": "2014-05-11T13:46:01.43Z",
+                "source_id": "3468493",
+                "event_id":
+                    "cea9a774-dfe8-4dfd-8a2a-bb2edc984407",
+                "return_code": "200"
+            },
+            {
+                "domain": "edu.byu",
+                "event_type": "Drop Class",
+                "source_dt": "2014-05-11T13:45:53.20Z",
+                "source_id": "58493852",
+                "return_code": "400"
+            },
+            {
+                "domain": "edu.byu",
+                "entity": "Independent Study",
+                "event_type": "Add Class",
+                "source_dt": "2014-05-11T13:45:53.20Z",
+                "source_id": "74838472",
+                "return_code": "401"
+            }
+        ]
+    }
+}
