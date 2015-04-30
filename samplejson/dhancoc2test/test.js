01a209bb3e95b9287e3e4174f66e013701d72b0b
dhancoc2 <daniel_hancock@byu.edu>
2015-04-30 11:50:34 -0600

    Sample test Json script

--- samplejson/dhancoc2test/test.js
+++ samplejson/dhancoc2test/test.js
@@ -1,1 +1,79 @@
-Hello World!
+{
+    "relationshipsSaved": "7",
+    "spouse": "Y",
+    "relationshipInformation": {
+        "class": [
+            {
+            "preferredName": "Bradley Harrop",
+            "relationship": "Spouse",
+            "maritalStatus": "Married",
+            "age": "64",
+            "gender": "M",
+            "studentRole": "Former Student",
+            "employeeRole": "Non-Employee",
+            "personID": "468203232"
+            },
+            {
+            "preferredName": "Dara Harrop",
+            "relationship": "Child",
+            "maritalStatus": "Single",
+            "age": "43",
+            "gender": "F",
+            "studentRole": "Former Student",
+            "employeeRole": "Non-Employee",
+            "personID": "422201222"
+            },
+            {
+            "preferredName": "Nathan Harrop",
+            "relationship": "Child",
+            "maritalStatus": "Single",
+            "age": "42",
+            "gender": "M",
+            "studentRole": "Non-Matriculated",
+            "employeeRole": "Non-Employee",
+            "personID": "423201222"
+            },
+            {
+            "preferredName": "Camille Harrop",
+            "relationship": "Child",
+            "maritalStatus": "Single",
+            "age": "40",
+            "gender": "F",
+            "studentRole": "Non-Matriculated",
+            "employeeRole": "Non-Employee",
+            "personID": "426201222"
+            },
+            {
+            "preferredName": "J. Phil Harrop",
+            "relationship": "Child",
+            "maritalStatus": "Married",
+            "age": "37",
+            "gender": "M",
+            "studentRole": "Former Student",
+            "employeeRole": "Non-Employee",
+            "personID": "431201222"
+            },
+            {
+            "preferredName": "Laurel Harrop",
+            "relationship": "Child",
+            "maritalStatus": "Single",
+            "age": "35",
+            "gender": "F",
+            "studentRole": "Former Student",
+            "employeeRole": "Non-Employee",
+            "personID": "442201222"
+            },
+            {
+            "preferredName": "Tessa Ward",
+            "relationship": "Child",
+            "maritalStatus": "Married",
+            "age": "32",
+            "gender": "F",
+            "studentRole": "Former Student",
+            "employeeRole": "STD/PT/TRM",
+            "personID": "441201222"
+            }
+        ]
+    },
+    "maximumAllowedRelationships": "20"
+}
