2d4a003f562e1efdd759be872e90e2d5914db4b9
mhailstone <matthew.hailstone@gmail.com>
2015-05-01 14:46:01 -0600

    Added summaries and webhooks to eventhub API

---
+++ samplejson/eventhub/eventhub-12.js
@@ -0,0 +1,22 @@
+{
+    "webhooks": {
+        "webhook": [
+            {
+                "identity_id": "388211692",
+                "identity_name": "HR Benefits System",
+                "endpoint": "https://hr.byu.edu/example/endpoint",
+                "push_option": "Push Message",
+                "security_option": "Public Key/Private Key Signing",
+                "content_type": "application/json"
+            },
+            {
+                "identity_id": "388219162",
+                "identity_name": "HR Student Employment System",
+                "endpoint": "https://hr.byu.edu/example/endpoint2",
+                "push_option": "Push Message",
+                "security_option": "Public Key/Private Key Signing",
+                "content_type": "application/xml"
+            }
+        ]
+    }
+}
