c90ebe32bac76630f600f1c06d0f1d302587c2a2
mhailstone <matthew.hailstone@gmail.com>
2015-05-01 14:00:17 -0600

    Added subscriptions to event hub API

---
+++ samplejson/eventhub/eventhub-9.js
@@ -0,0 +1,20 @@
+{
+    "subscriptions": {
+        "subscription": [
+            {
+                "eca_identity_id": "585241162",
+                "eca_identity_name": "HR Benefits",
+                "domain": "edu.byu",
+                "entity": "AIM WaitLising",
+                "event_type": "Add Class"
+            },
+            {
+                "eca_identity_id": "585241162",
+                "eca_identity_name": "HR Benefits",
+                "domain": "edu.byu",
+                "entity": "AIM WaitLising",
+                "event_type": "Add Class"
+            }
+        ]
+    }
+}
