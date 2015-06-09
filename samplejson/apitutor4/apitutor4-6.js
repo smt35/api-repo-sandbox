69424b175e3f62a4c7e3719318987615270b1503
dwa93 <david.byu@rlaf.org>
2015-06-09 16:22:22 -0600

    Updated File

--- samplejson/apitutor4/apitutor4-6.js
+++ samplejson/apitutor4/apitutor4-6.js
@@ -1,10 +1,6 @@
-{
-  "odometer_logs": [
-    { "vin": "73",
-      "read_date": "2014-05-11",
-      "miles": 32459,
-      "read_by_id": "586586586",
-      "read_by_name": "Daffy Duck"
-    }
-  ]
+{ "vin": "73",
+  "read_date": "2014-05-11",
+  "miles": 32459,
+  "read_by_id": "586586586",
+  "read_by_name": "Daffy Duck"
 }
