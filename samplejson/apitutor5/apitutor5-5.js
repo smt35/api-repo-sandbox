3ff40cc8b12b82acd2e061892d6c6c63e53aed36
dwa93 <david.byu@rlaf.org>
2015-06-18 16:57:29 -0600

    Uploading APITutor5

---
+++ samplejson/apitutor5/apitutor5-5.js
@@ -0,0 +1,20 @@
+[
+    { "vin": "73",
+      "read_date": "2014-05-11",
+      "miles": 32459,
+      "read_by_id": "586586586",
+      "read_by_name": "Daffy Duck"
+    },
+    { "vin": "73",
+      "read_date": "2014-06-08",
+      "miles": 33782,
+      "read_by_id": "586586586",
+      "read_by_name": "Daffy Duck"
+    },
+    { "vin": "73",
+      "read_date": "2014-07-12",
+      "miles": 35121,
+      "read_by_id": "783783783",
+      "read_by_name": "Daisy Duck"
+    }
+ ]
